85a96470cede26009c8dad69120fc46472f7f6c8
kelefhatul <kostyantinromanov@gmail.com>
2016-12-01 22:01:44 +0200

    task04

--- big01/View.java
+++ big01/View.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level32.lesson15.big01;
 
+import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,7 +13,7 @@ public class View extends JFrame implements ActionListener {
     private Controller controller;
     private JTabbedPane tabbedPane = new JTabbedPane(); //panel with two tabs
     private JTextPane htmlTextPane = new JTextPane(); //component for visual html redacting
-    private JEditorPane plainTextPane = new JEditorPane(); //component for text html redacting 
+    private JEditorPane plainTextPane = new JEditorPane(); //component for text html redacting
 
     public Controller getController() {
         return controller;
@@ -31,6 +33,30 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init(){
+        initGui(); //4.3.1
+        //4.3.2
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        //4.3.3
+        setVisible(true);
+
 
     }
+    //4.1 start
+    public void initMenuBar(){
+
+    }
+
+    public void initEditor(){
+
+    }
+    //4.1 end
+    //4.2
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+
 }
